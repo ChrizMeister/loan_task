db74c16fb0fb2c34f3c49f149971c4bd522880d7
Chris <chris.sommerville7@gmail.com>
2024-12-18 19:30:32 +0000

    readme + view edited loan details

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple Loan Interest Calculator
+
+This is a simple loan interest calculator that allows you to add, edit, and view simple interest loans.
+
+## Usage
+
+Install [rust](https://www.rust-lang.org/tools/install)
+
+Run the program
+```bash
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -20,16 +20,7 @@ fn main() -> Result<(), String> {
                 });
             }
             CliChoice::EditLoan => {
-                let loan_index = match select_loan_to_edit(&history) {
-                    Ok(loan_index) => loan_index,
-                    Err(e) => {
-                        println!("{}", e);
-                        continue;
-                    }
-                };
-                let curr_loan_input = &history[loan_index].loan_input;
-                let updated_loan_input = get_loan_input(curr_loan_input.id, Some(curr_loan_input))?;
-                history[loan_index].loan_input = updated_loan_input;
+                edit_loan(&mut history)?;
             }
             CliChoice::Exit => {
                 break;
@@ -41,7 +32,11 @@ fn main() -> Result<(), String> {
 
 fn add_new_loan(idx: usize) -> Result<LoanInput, String> {
     let loan_input = get_loan_input(idx, None)?;
+    analyse_loan(&loan_input)?;
+    Ok(loan_input)
+}
 
+fn analyse_loan(loan_input: &LoanInput) -> Result<(), String> {
     let accrual_date = get_accrual_date()?.unwrap_or(loan_input.end_date);
 
     let loan_output = get_loan_output(&loan_input, accrual_date);
@@ -63,5 +58,21 @@ fn add_new_loan(idx: usize) -> Result<LoanInput, String> {
         loan_input.currency.code()
     );
     let _ = get_console_input("Enter to continue...")?;
-    Ok(loan_input)
+
+    Ok(())
+}
+
+fn edit_loan(history: &mut Vec<LoanHistory>) -> Result<(), String> {
+    let loan_index = match select_loan_to_edit(&history) {
+        Ok(loan_index) => loan_index,
+        Err(e) => {
+            println!("{}", e);
+            return Err(e);
+        }
+    };
+    let curr_loan_input = &history[loan_index].loan_input;
+    let updated_loan_input = get_loan_input(curr_loan_input.id, Some(curr_loan_input))?;
+    analyse_loan(&updated_loan_input)?;
+    history[loan_index].loan_input = updated_loan_input;
+    Ok(())
 }
